3d64fea5624278998ba9033a4df6542c6c571f95
Emir Karşıyakalı <emirkarsiyakali@gmail.com>
2020-05-29 02:05:36 +0300

    first commit🎉

---
+++ assets/js/main.js
@@ -0,0 +1,30 @@
+$(function () {
+  $(".menu-toggle").on("click", function () {
+    $(".mobile-menu").css({ display: "flex" });
+  });
+  $(".close-menu").on("click", function () {
+    $(".mobile-menu").css({ display: "none" });
+  });
+  $(".mobile-menu ul li a").on("click", function () {
+    $(".mobile-menu").css({ display: "none" });
+  });
+  $(".owl-carousel").owlCarousel({
+    loop: true,
+    margin: 10,
+    autoWidth: false,
+    nav: true,
+    items: 5,
+    navText: [
+      "<img class='chevron' src='assets/img/left.svg' alt='left' />",
+      "<img class='chevron' src='assets/img/right.svg' alt='right' />",
+    ],
+    responsive : {
+      0 : {
+          items: 2,
+      },
+      768 : {
+          items: 5
+      }
+  }
+  });
+});

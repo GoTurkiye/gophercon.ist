6f7fc7e90f9b6a58dff10dabb684bf5b3764187a
Mücahit Cücen <mucahitcucen@gmail.com>
2020-06-03 02:43:34 +0300

    vue initial commit

--- src/App.vue
+++ src/App.vue
@@ -1,28 +1,256 @@
 <template>
-  <div id="app">
-    <img alt="Vue logo" src="./assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
-  </div>
+    <div id="app">
+        <div class="mobile-menu">
+            <ul class="menu">
+                <li><a href="#about">{{ $t('About') }}</a></li>
+                <li><a href="#speakers">{{ $t('Speakers') }}</a></li>
+                <li><a href="#schedule">{{ $t('Schedule') }}</a></li>
+                <li><a href="#sponsors">{{ $t('Sponsors') }}</a></li>
+                <li><a href="#ticket" class="button">{{ $t('Tickets') }}</a></li>
+            </ul>
+            <a href="javascript::void(0)" class="close-menu">x</a>
+        </div>
+
+        <a href="javascript::void(0)" class="menu-toggle">
+            <img src="assets/img/open-menu.svg" alt="open mobile menu"/>
+        </a>
+
+        <section id="slider" class="slider">
+            <img class="sliderbg" src="assets/img/slider.png" alt="slider"/>
+            <img class="sliderbgmobile" src="assets/img/slider.png" alt="slider"/>
+
+            <div class="header-absolute">
+                <div class="container">
+                    <header>
+                        <a href="index.html" class="logo"
+                        ><img src="assets/img/gophercon.png" alt="logo"
+                        /></a>
+                        <ul class="menu">
+                            <li>
+                                <a href="javascript::void()" class="button menu-toggle">
+                                    <img
+                                            src="assets/img/open-menu-white.svg"
+                                            alt="open mobile menu"
+                                    />
+                                </a>
+                            </li>
+                        </ul>
+                    </header>
+                </div>
+            </div>
+            <div class="announcement-aboslute">
+                <h1>{{ $t('25th, July 2020') }}</h1>
+                <p>{{ $t('GopherCon is focused on the Go programming language') }}</p>
+                <div class="buttons">
+                    <a class="btn btn-custom" href="#">{{ $t('Call for Papers') }}</a>
+                    <a class="btn btn-custom" href="#">{{ $t('Register') }}</a>
+                </div>
+            </div>
+        </section>
+        <section class="about" id="about">
+            <div class="container">
+                <div class="about-flex">
+                    <img src="assets/img/gopher-hi.png" alt="gopher" class="gopher"/>
+                    <h2 class="title">{{ $t('ABOUT CONFERENCE') }}</h2>
+                    <h2>
+                        {{ $t('GopherCon Turkey brings the Turkish Go community together through its events throughout the world. The conference will be held on July 25 at Online. 1000+ Gophers will attend the event.') }}
+                    </h2>
+                </div>
+            </div>
+        </section>
+        <section class="speakers" id="speakers">
+            <h2 class="title">{{ $t('SPEAKERS') }}</h2>
+            <div class="container">
+                <div class="row">
+                    <div class="col-12 col-md-4">
+                        <div class="profile">
+                            <img height="180px" src="assets/img/gopher-05.png" alt="avatar" class="avatar"/>
+                            <p class="name">{{ $t('You?') }}</p>
+                            <!-- <p class="description">
+                              Gopher
+                            </p> -->
+                            <ul class="social">
+                                <li>
+                                    <a href="#"
+                                    ><img src="assets/img/twitter.svg" alt="twitter"
+                                    /></a>
+                                </li>
+                                <li>
+                                    <a href="#"
+                                    ><img src="assets/img/github.svg" alt="github"
+                                    /></a>
+                                </li>
+                                <li>
+                                    <a href="#"
+                                    ><img src="assets/img/linkedin.svg" alt="linkedin"
+                                    /></a>
+                                </li>
+                            </ul>
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </section>
+        <section class="schedule" id="schedule">
+            <img
+                    src="assets/img/gopher-02.png"
+                    alt="gopher"
+                    class="gopher hide-mobile"
+            />
+            <h2 class="title">{{ $t('SCHEDULE') }}</h2>
+            <div class="container">
+                <div class="feed-out">
+                    <div class="feed">
+                        <div class="line-center">
+                            <div class="track-title flex1 hide-mobile">
+                                <p>{{ $t('Sponsor Track 1') }}</p>
+                                <img class="arrow" src="assets/img/arrow.svg" alt="arrow"/>
+                            </div>
+                            <div class="flex1 flex-center">
+                                <div class="timer">10:00 - {{ $t('Start') }}</div>
+                            </div>
+                            <div class="track-title flex1 hide-mobile">
+                                <p>{{ $t('Sponsor Track 2') }}</p>
+                                <img class="arrow" src="assets/img/arrow.svg" alt="arrow"/>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </section>
+        <section class="ticket" id="ticket">
+            <img
+                    src="assets/img/gopher-07.png"
+                    alt="gopher"
+                    class="gopher hide-mobile"
+            />
+            <h2 class="title">{{ $t('TICKETS') }}</h2>
+            <div class="container">
+                <div class="tables">
+                    <div class="table mt-4">
+                        <img
+                                src="assets/img/gopher-03.png"
+                                alt="gopher"
+                                class="gopher-free"
+                        />
+                        <div class="name">{{ $t('COME AND JOIN!') }}</div>
+                        <p class="price">{{ $t('Free') }}</p>
+                        <ul>
+                            <li>{{$t('Ticket guarrantees admission')}}</li>
+                        </ul>
+                        <a href="#" class="buy">{{$t('REGISTER')}}</a>
+                    </div>
+                    <div class="table table-red">
+                        <img
+                                src="assets/img/gopher-04.png"
+                                alt="gopher"
+                                class="gopher-support"
+                        />
+                        <div class="name">{{$t('SUPPORT CHARITY')}}</div>
+                        <p class="price"><small>TRY</small>0</p>
+                        <ul>
+                            <li>{{$t('Ticket guarrantees admission')}}</li>
+                        </ul>
+                        <a href="#" class="buy buy-red">{{$t('BUY NOW')}}</a>
+                    </div>
+                    <div class="table mt-4">
+                        <img
+                                src="assets/img/gopher-05.png"
+                                alt="gopher"
+                                class="gopher-community"
+                        />
+                        <div class="name">{{$t('SUPPORT CHARITY')}}</div>
+                        <p class="price"><small>TRY</small>0</p>
+                        <ul>
+                            <li>{{$t('Ticket guarrantees admission')}}</li>
+                        </ul>
+                        <a href="#" class="buy">{{$t('BUY NOW')}}</a>
+                    </div>
+                </div>
+            </div>
+        </section>
+        <section class="sponsors" id="sponsors">
+            <img
+                    src="assets/img/gopher-06.png"
+                    alt="gopher"
+                    class="gophers hide-mobile"
+            />
+            <h2 class="title">{{$t('SPONSORS')}}</h2>
+            <div class="container">
+                <div class="owl-carousel owl-theme">
+                    <div class="sponsor">
+                        <a href="#"
+                        ><img
+                                src="https://itsmoneo.com/assets/image/logo.svg"
+                                alt="sponsor"
+                        /></a>
+                    </div>
+                </div>
+                <p class="info">
+                    {{$t('Would you like to be the part of this great event?')}}
+                    <br/>
+                    <span v-html="$t('please_get_in_touch')"></span>
+                </p>
+            </div>
+        </section>
+
+        <footer>
+            <div class="container">
+                <div class="row">
+                    <div class="col-12 col-md-4">
+                    </div>
+                    <div class="col-12 col-md-4">
+                        <ul class="footer-menu">
+                            <li>
+                                <a target="_blank" href="https://golang.org/conduct"
+                                >{{$t('Code of Conduct')}}</a
+                                >
+                            </li>
+                            <li>
+                                <a href="mailto:support@kommunity.com?Subject=GopherCon Contact"
+                                >{{$t('Contact Us')}}</a
+                                >
+                            </li>
+                        </ul>
+                    </div>
+                    <div class="col-12 col-md-4">
+                        <ul class="footer-social-menu">
+                            <li>
+                                <a href="#" title="twitter"
+                                ><img src="assets/img/twitter-white.svg" alt="twitter"
+                                /></a>
+                            </li>
+                            <li>
+                                <a href="#" title="github"
+                                ><img src="assets/img/github-white.svg" alt="github"
+                                /></a>
+                            </li>
+                            <li>
+                                <a href="#" title="kommunity"
+                                ><img src="assets/img/kommunity.svg" alt="kommunity"
+                                /></a>
+                            </li>
+                        </ul>
+                    </div>
+                </div>
+            </div>
+        </footer>
+    </div>
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
+    export default {
+        name: 'App',
+        mounted() {
+            let url = document.location.href
+            let indexOfSharp = url.indexOf('#')
+            let lang = url.substring(indexOfSharp + 1)
 
-export default {
-  name: 'App',
-  components: {
-    HelloWorld
-  }
-}
+            this.$i18n.locale = lang
+        }
+    }
 </script>
 
 <style>
-#app {
-  font-family: Avenir, Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-  margin-top: 60px;
-}
+
 </style>
--- src/main.js
+++ src/main.js
@@ -1,8 +1,27 @@
 import Vue from 'vue'
 import App from './App.vue'
+import VueI18n from 'vue-i18n'
+import en from './languages/en.json'
+import tr from './languages/tr.json'
 
+Vue.use(VueI18n)
 Vue.config.productionTip = false
 
+const languages = {
+  en: en,
+  tr: tr
+}
+
+const messages = Object.assign(languages)
+
+const i18n = new VueI18n({
+  locale: 'en',
+  fallbackLocale: 'en',
+  messages
+})
+
+
 new Vue({
+  i18n,
   render: h => h(App),
 }).$mount('#app')

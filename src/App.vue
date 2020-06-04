ef842fea8adc2a05311984adfb25a8a7b638ba82
Mücahit Cücen <mucahitcucen@gmail.com>
2020-06-04 17:22:10 +0300

    fixed twitter card

--- src/App.vue
+++ src/App.vue
@@ -273,7 +273,7 @@
                     },
                     {
                         'name': 'twitter:card',
-                        'content': 'summary',
+                        'content': 'summary_large_image',
                         'vmid': 'twitter:card'
                     },
                     {

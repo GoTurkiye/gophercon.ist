e61bc1d5399761e89307df317d883f4360d5996b
Batuhan Apaydın <batuhan.apaydin@trendyol.com>
2021-10-25 22:53:18 +0300

    feat: add speakers, sponsors, tickets, etc
    Signed-off-by: Batuhan Apaydın <batuhan.apaydin@trendyol.com>

--- assets/js/main.js
+++ assets/js/main.js
@@ -38,3 +38,41 @@ $(function () {
     );
   });
 });
+
+const API_URL = 'https://api.kommunity.com/api/v3/goturkiye/events/gophercon-turkey-1c8eab1d'
+const API_TICKETS_URL = 'https://api.kommunity.com/api/v1/tickets?event_id=2e8bca4e-5d18-49ca-809f-322383121a98'
+
+var App = new Vue({
+    el: '#app',
+    data: {
+        days: [],
+        speakers: [],
+        sessions: [],
+        tickets: [],
+        sponsors: [],
+        tracks: []
+    },
+    mounted() {
+        this.getData()
+    },
+    methods: {
+        getData: function () {
+            axios.get(API_URL).then(response => {
+                  console.log(response.data.data)
+                  this.speakers = response.data.data.speakers;
+                  this.sponsors = response.data.data.sponsors;
+                  console.log("schedule:",response.data.data.schedule );
+                  this.tracks = response.data.data.schedule[0].tracks;
+                }
+            );
+
+
+
+            axios.get(API_TICKETS_URL).then(response => {
+              console.log(response.data.data)
+              this.tickets = _.chain(response.data.data).toPairs().sortBy(0).take(3).fromPairs().value()
+            }
+        );
+        },
+    } 
+});
